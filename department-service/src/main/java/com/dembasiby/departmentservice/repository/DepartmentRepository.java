fc20b2fb75f78518f08cef7180bb523dc993ecd3
Demba Siby <dembasiby@gmail.com>
2024-04-02 20:27:57 +0000

    Added department entity, repository and DTO

---
+++ department-service/src/main/java/com/dembasiby/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.dembasiby.departmentservice.repository;
+
+import com.dembasiby.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
